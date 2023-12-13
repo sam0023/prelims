653909aba877138417958fc3d70feb2267a90e58
sameer <sdsameer61116@gmail.com>
2023-12-13 08:11:46 +0530

    a

--- src/Subjects/Sociology/CrimeAgainstWomen/index.js
+++ src/Subjects/Sociology/CrimeAgainstWomen/index.js
@@ -1,2 +1,135 @@
-const CrimeAgainstWomen = [];
+const CrimeAgainstWomen = [
+	{
+		questionText: 'Which of the following kind of cases are registered high against women in India?',
+		ans: ['Cruelty by husband and relatives'],
+	},
+	{
+		questionText: 'Which of the  State has recorded more no of violent incidents on women in India as per NCRB 2021',
+		ans: ['U.P'],
+	},
+	{
+		questionText: 'The Protection of Women from Domestic Violence Act',
+		ans: ['2005'],
+	},
+	{
+		questionText: 'The commission of Sati(prevention) Act',
+		ans: ['1987'],
+	},
+	{
+		questionText: 'The Prohibition of Child Marriage Act',
+		ans: ['2006'],
+	},
+	{
+		questionText: 'Hindu Widow Remarriage Act ',
+		ans: ['1856'],
+	},
+	{
+		questionText: 'Medical Termination of Pregnancy Act ',
+		ans: ['1971'],
+	},
+	{
+		questionText: 'Suppression of Immoral Traffic in Women and Girls Act(SITA Act)',
+		ans: ['1956 '],
+	},
+	{
+		questionText: 'Sharadha Act came into force on',
+		ans: ['April 1, 1930'],
+	},
+	{
+		questionText: 'Dowry Prohibition Act was enacted in the',
+		ans: ['1961'],
+	},
+	{
+		questionText: 'Dowry Prohibition Act was amended in the year',
+		ans: ['1986'],
+	},
+	{
+		questionText: 'Prohibition of Child Marriage Act was enacted in the year?',
+		ans: ['2006'],
+	},
+	{
+		questionText: 'What is the punishment under Prohibition of Child Marriage Act?',
+		ans: ['2 years of imprisonment'],
+	},
+	{
+		questionText: 'Hindu Marriage Act was enacted in the year?',
+		ans: ['1995'],
+	},
+	{
+		questionText: 'The Muslim women (Protection of Rights on Marriage) Act was introduced in the year?',
+		ans: ['2019'],
+	},
+	{
+		questionText: 'Practicing Triple Talaq is prohibited in',
+		ans: ['2018'],
+	},
+	{
+		questionText: 'Shahara Banu case is related to',
+		ans: ['Compensation'],
+	},
+	{
+		questionText: 'Punishment for practicing Triple Talaq is',
+		ans: ['3 years of imprisonment'],
+	},
+	{
+		questionText: 'The Hindu Adoption and Maintenance Act was enacted in the year?',
+		ans: ['1958'],
+	},
+	{
+		questionText: 'The Indian Divorce Act was introduced in the year?',
+		ans: ['1869'],
+	},
+	{
+		questionText: 'Dissolution of Muslim Marriage Act was enacted in the year?',
+		ans: ['1939'],
+	},
+	{
+		questionText: 'PCPNDT Act was introduced in the year',
+		ans: ['1993'],
+	},
+	{
+		questionText: 'PCPNDT Act full form',
+		ans: ['Pre-Conception and Pre-Natal Diagnostic Techniques (PCPNDT) Act '],
+	},
+	{
+		questionText: 'Which IPC section deals with Cruelty by Husband',
+		ans: ['IPC 498A'],
+	},
+	{
+		questionText: 'The Indecent Representation of Women (Prohibition) Act, was enacted in the year',
+		ans: ['1986'],
+	},
+	{
+		questionText: 'The Protection of Women from Domestic Violence Act was enacted in the year?',
+		ans: ['2005'],
+	},
+	{
+		questionText: 'The Protection of Women from Domestic Violence Act was come into force from?',
+		ans: ['26th Oct, 2006'],
+	},
+	{
+		questionText: 'which committe is formed on Nirbhaya incident',
+		ans: ['JJ Verma'],
+	},
+	{
+		questionText: 'Sexual Harassment of Women at Workplace (Prevention, Prohibition and Redressa Act was introduced in the year?',
+		ans: ['2013'],
+	},
+	{
+		questionText: 'Vishaka Guidelines are related to',
+		ans: ['Child marriage'],
+	},
+	{
+		questionText: 'Which IPC section deals with Dowery Death',
+		ans: ['IPC 304B'],
+	},
+	{
+		questionText: 'Which of the following Act is related to mechanism of Internal Complaints Committee?',
+		ans: ['Sexual Harassment at Work Place Act'],
+	},
+	{
+		questionText: 'When did International Day for the Elimination of Violence against Women was celebrated?',
+		ans: ['November 25th'],
+	},
+];
 export default CrimeAgainstWomen;
